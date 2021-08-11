2e2d3b41962347b20e1d1224e226cac15d39715b
Steven Roland <steven@mojave.io>
2021-08-11 12:03:58 -0400

    Add some typical pages and routes for each

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,23 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
+import Login from '../views/Login.vue'
+import Account from '../views/Account.vue'
 
 const routes = [
   {
     path: '/',
     name: 'Home',
     component: Home
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    component: Login
+  },
+  {
+    path: '/account',
+    name: 'Account',
+    component: Account
   }
 ]
 

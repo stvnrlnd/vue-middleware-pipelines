1db3b0b6b30c2d2f743f13bf4ee1c1dabfc7e814
Steven Roland <steven@mojave.io>
2021-08-11 13:00:13 -0400

    Set up middleware pipeline

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,14 @@
 import { createRouter, createWebHistory } from 'vue-router'
+
 import Home from '../views/Home.vue'
 import Login from '../views/Login.vue'
 import Dashboard from '../views/Dashboard.vue'
+import Billing from '../views/Billing.vue'
+
+import middlewarePipeline from './middlewarePipeline'
 
 import auth from '@/middleware/auth'
+import subscribed from '@/middleware/subscribed'
 
 const routes = [
   {
@@ -20,13 +25,42 @@ const routes = [
     path: '/dashboard',
     name: 'Dashboard',
     component: Dashboard,
-    beforeEnter: auth
+    meta: {
+      middleware: [auth]
+    }
+  },
+  {
+    path: '/billing',
+    name: 'Billing',
+    component: Billing,
+    meta: {
+      middleware: [auth, subscribed]
+    }
   },
 ]
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes
+})
+
+router.beforeEach((to, from, next) => {
+
+  const middleware = to.meta.middleware
+
+  const context = {to, from, next}
+
+  if (! middleware) {
+    return next()
+  }
+
+  return middleware[0]({ 
+    ...context, 
+    next: middlewarePipeline(context, middleware, 1)
+  })
+
+  next()
+
 })
 
 export default router
